8d0cba6b915875751dfbafb0933c7738927ae732
cbartram <cbartram3@gmail.com>
2020-04-30 15:54:44 -0400

    Added watch landmark detail

---
+++ Landmarks/WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by Christian Bartram on 4/30/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
