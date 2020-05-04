db3a08d288d3f9d76b49c4e5a8f3f53f92fde99d
cbartram <cbartram3@gmail.com>
2020-05-04 16:42:35 -0400

    Added streaming samples from HealthkIT

--- Landmarks/WatchLandmarks Extension/ContentView.swift
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -11,7 +11,24 @@ import SwiftUI
 import HealthKit
 
 struct ContentView: View {
-        
+    
+    func getHealthValues(store: HKHealthStore, sampleType: HKSampleType, completion: @escaping ([HKQuantitySample]) -> Void) {
+        let query = HKSampleQuery(sampleType: sampleType, predicate: nil, limit: Int(HKObjectQueryNoLimit), sortDescriptors: nil) {
+            query, results, error in
+
+            guard let samples = results as? [HKQuantitySample] else {
+                return
+            }
+            
+            // Basically a callback
+            DispatchQueue.main.async {
+                completion(samples)
+            }
+        }
+
+        store.execute(query)
+    }
+
     var body: some View {
         
        print("Setting up health store...")
@@ -34,55 +51,71 @@ struct ContentView: View {
         
         do {
             let urlSession = URLSession(configuration: .default)
-            let webSocketTask = urlSession.webSocketTask(with: URL(string: "ws://localhost:8080/ws/greetings")!)
+            let webSocketTask = urlSession.webSocketTask(with: URL(string: "ws://localhost:8080/ws/metrics")!)
+            print("Opening Websocket connection to url: ws://localhost:8080/ws/metrics")
             webSocketTask.resume()
 
-            let message = URLSessionWebSocketTask.Message.string("Hello Socket")
-            webSocketTask.send(message) { error in
-                if let error = error {
-                    print("WebSocket sending error: \(error)")
-                }
-            }
-
-            let configuration: HKWorkoutConfiguration = HKWorkoutConfiguration()
+            let configuration = HKWorkoutConfiguration()
+            configuration.activityType = .running
+            configuration.locationType = .outdoor
+            
             let session: HKWorkoutSession = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
             let builder: HKLiveWorkoutBuilder = session.associatedWorkoutBuilder()
             
+            // Set the datasource for the workout
             builder.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore, workoutConfiguration: configuration)
-                    
-            guard let sampleType = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
+             
+            // Start the workout
+            session.startActivity(with: Date())
+            builder.beginCollection(withStart: Date()) { (success, error) in
+               print("Beginning workout metric collection...")
+            }
+            
+            guard let sampleType = HKSampleType.quantityType(forIdentifier: .heartRate) else {
                 fatalError("*** This method should never fail ***")
             }
             
-            let query = HKSampleQuery(sampleType: sampleType, predicate: nil, limit: Int(HKObjectQueryNoLimit), sortDescriptors: nil) {
-                query, results, error in
+//            for sample in samples {
+//                         // Process each sample here.
+//                         let sampleValue = sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))
+//                         let roundedValue = Double( round( 1 * sampleValue ) / 1 )
+//                         print("Publishing value: ", roundedValue)
+            
+            
+            let query = HKObserverQuery(sampleType: sampleType, predicate: nil) { (query, completionHandler, errorOrNil) in
                 
-                guard let samples = results as? [HKQuantitySample] else {
+                if let error = errorOrNil {
+                    print("Error thrown when executing HK Observer query: ", error)
                     return
                 }
+                   
                 
-                for sample in samples {
-                    // Process each sample here.
-                    let sampleValue = sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))
-                    let roundedValue = Double( round( 1 * sampleValue ) / 1 )
-//                    print("Rounded Value: ", roundedValue)
+                let query = HKAnchoredObjectQuery(type: sampleType, predicate: nil, anchor: nil, limit: HKObjectQueryNoLimit) { (query, samplesOrNil, deletedObjectsOrNil, newAnchor, errorOrNil) in
+                    
+                     guard let samples = samplesOrNil as? [HKQuantitySample] else {
+                           // Handle any errors here.
+                           return
+                       }
+                                    
+                    for sample in samples {
+                        let sampleValue = sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))
+                        let roundedValue = Double( round( 1 * sampleValue ) / 1 )
+                        print("Publishing value: ", roundedValue)
+                        let message = URLSessionWebSocketTask.Message.string("{ \"metric\": \"heartRate\", \"value\": \"" + String(format:"%.1f", roundedValue) + "\"}")
+                        webSocketTask.send(message) { error in
+                            if let error = error {
+                                print("WebSocket couldn’t send message because: \(error)")
+                             }
+                        }
+                    }
                 }
+                healthStore.execute(query)
             }
             
             healthStore.execute(query)
-            
-            
-            let now: Date = Date();
-            // Start the workout session and begin data collection.
-            session.startActivity(with: now)
-            builder.beginCollection(withStart: now) { (success, error) in
-              print("Collecting Health Metrics...")
-            }
           } catch {
-
+            print("There was an error starting the workout")
           }
-        
-       print("Returning Landmark List View")
        return LandmarkList {
         WatchLandmarkDetail(landmark: $0)
        }.environmentObject(UserData())
